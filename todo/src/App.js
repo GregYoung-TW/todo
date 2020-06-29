2175df2d845836187a193b350aa3b3e12dcc396b
Gregory Young <gregoryjryoung@gmail.com>
2020-06-29 16:56:30 -0400

    First commit

---
+++ todo/src/App.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+
+class TodoList extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    };
+  }
+  
+  
+  handleChange = event =>{
+    console.log(event.target.value)
+  
+  };
+  
+  addItem = event =>{
+    event.preventDefault();
+    console.log("addItem Method fired")
+  
+  };
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  render(){
+    return(
+      <div className='container'>
+       <form onSubmit={this.addItem}>
+         <label htmlFor="taskName">Task Name:</label>
+         <input onChange={this.handleChange} name="taskName"type="text"placeholder="Add todo here!" />
+         <button type="submit"> Add Task</button>
+         </form>
+         </div>
+    )
+  }
+}
+
+export default TodoList;
